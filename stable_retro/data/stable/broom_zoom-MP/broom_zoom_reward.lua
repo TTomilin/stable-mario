a49bb3ed34c6f35e50f414691355a285ade745e7
FabriceTUe <f.e.kusters@student.tue.nl>
2024-03-04 19:18:51 +0100

    slightly modified custom reward function

--- stable_retro/data/stable/broom_zoom-MP/broom_zoom_reward.lua
+++ stable_retro/data/stable/broom_zoom-MP/broom_zoom_reward.lua
@@ -4,12 +4,9 @@ time = 0
 function reward()
     local score_rew = data.broom_zoom_score - previous_score
     previous_score = data.broom_zoom_score
-    
-    if score_rew > 0 then
-        print(score_rew)
-    end
+
     if data.game_over == 1 then
-        print(string.format("episode reward: %d", previous_score))
+        print(string.format("episode reward: %d", previous_score)) -- just for debugging: wanted to see if average episode score matches the one displayed by stable-baselines
     end
 
     return score_rew;
