c2309cab0e32e1af6ab1efb9b35413892337dd0f
FabriceTUe <f.e.kusters@student.tue.nl>
2024-03-22 08:29:21 +0100

    added ShowObservationWrapper and new config file

--- scripts/config.py
+++ scripts/config.py
@@ -14,5 +14,21 @@ CONFIG = {
         'clip_reward': False,
         'resize': (80, 72),
         'timesteps': 1000000,
+    },
+    'amplifried': {
+        'actions': [['UP'], ['DOWN'], ['LEFT'], ['RIGHT'], []],
+        'game_env': 'amplifried-MP',
+        'state': 'Level1',
+        'clip_reward': False,
+        'resize': (160, 144),
+        'timesteps': 1000000,
+    },
+    'bunny_belt': {
+        'actions': [['UP'], ['DOWN'], ['LEFT'], ['RIGHT'], ['A'], []],
+        'game_env': 'bunny_belt-MP',
+        'state': 'Level1',
+        'clip_reward': False,
+        'resize': (160, 144),
+        'timesteps': 1000000
     }
 }
--- wrappers/observation.py
+++ wrappers/observation.py
@@ -1,7 +1,12 @@
+from __future__ import annotations
 from typing import Any, SupportsFloat
 
 import gymnasium
 from gymnasium.core import WrapperObsType, WrapperActType
+
+import numpy as np
+import gymnasium as gym
+import pylab as pl;
 
 
 class Rescale(gymnasium.Wrapper):
@@ -18,3 +23,56 @@ class Rescale(gymnasium.Wrapper):
     def step(self, action: WrapperActType) -> tuple[WrapperObsType, SupportsFloat, bool, bool, dict[str, Any]]:
         state, reward, done, truncated, info = self.env.step(action)
         return state / 255. * 2 - 1, reward, done, truncated, info
+    
+class ShowObservation(gym.ObservationWrapper, gym.utils.RecordConstructorArgs):
+    """Resize the image observation.
+
+    This wrapper works on environments with image observations. More generally,
+    the input can either be two-dimensional (AxB, e.g. grayscale images) or
+    three-dimensional (AxBxC, e.g. color images). This resizes the observation
+    to the shape given by the 2-tuple :attr:`shape`.
+    The argument :attr:`shape` may also be an integer, in which case, the
+    observation is scaled to a square of side-length :attr:`shape`.
+
+    Example:
+        >>> import gymnasium as gym
+        >>> from gymnasium.wrappers import ResizeObservation
+        >>> env = gym.make("CarRacing-v2")
+        >>> env.observation_space.shape
+        (96, 96, 3)
+        >>> env = ResizeObservation(env, 64)
+        >>> env.observation_space.shape
+        (64, 64, 3)
+    """
+
+    def __init__(self, env: gym.Env) -> None:
+        """Shows a graphical representation of the AIs observations
+
+        Args:
+            env: The environment to apply the wrapper
+        """
+        gym.utils.RecordConstructorArgs.__init__(self)
+        gym.ObservationWrapper.__init__(self, env)
+
+    def observation(self, observation):
+        """Shows a graphical representation of the AI's inputs under the applied wrappers
+
+        Args:
+            None
+
+        Returns:
+            Unchanged observations
+        """
+        try:
+            self.animate_observations(observations=observation)
+        except KeyboardInterrupt:
+            raise KeyboardInterrupt;
+        return observation
+
+    def animate_observations(self, observations):
+        pl.imshow(observations)
+        try:
+            pl.pause(10**-6)
+        except KeyboardInterrupt:
+            return;
+        pl.draw()
