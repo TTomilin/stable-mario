47990d6eba296a81074fdfd01b04c1432d42b2f6
FabriceTUe <f.e.kusters@student.tue.nl>
2024-04-26 13:09:46 +0200

    changed run_minigame

--- scripts/run_minigame.py
+++ scripts/run_minigame.py
@@ -52,7 +52,7 @@ def main(cfg: argparse.Namespace):
         env = ClipRewardEnv(env)
     if cfg.record:
         video_folder = f"{log_dir}/videos"
-        env = RecordVideo(env=env, video_folder=video_folder, episode_trigger=lambda x: x % cfg.record_every == 0)
+        env = RecordVideo(env=env, video_folder=video_folder, step_trigger=lambda x: x % cfg.record_every == 0)
 
     # Create a callback to save best model
     eval_env = Monitor(copy(env))
@@ -103,7 +103,7 @@ if __name__ == '__main__':
     arg("--render_mode", default="rgb_array", choices=["human", "rgb_array"], help="Render mode")
     arg("--load_state", type=str, default=None, help="Path to the game save state to load")
     arg("--record", default=False, action='store_true', help="Whether to record gameplay videos")
-    arg("--record_every", type=int, default=100, help="Record gameplay video every n episodes")
+    arg("--record_every", type=int, default=20, help="Record gameplay video every n updates")
     arg("--store_model", default=False, action='store_true', help="Whether to record gameplay videos")
     arg("--store_every", type=int, default=100, help="Save model every n episodes")
     arg("--skip_frames", default=False, action='store_true', help="Whether to skip frames")
