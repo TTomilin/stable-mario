e813195e561f950ea92a6e2c3fff9d92949ee016
FabriceTUe <f.e.kusters@student.tue.nl>
2024-05-10 15:58:35 +0200

    fixed var scope issue

--- scripts/run_minigame.py
+++ scripts/run_minigame.py
@@ -64,6 +64,7 @@ def main(cfg: argparse.Namespace):
                                  eval_freq=cfg.store_every, deterministic=True, render=False)
 
     # Create the model
+    model = None
     if cfg.model == "PPO":
         model = PPO(policy='CnnPolicy', env=env, device=device, ent_coef=cfg.ent_coeff,
                     learning_rate=cfg.learning_rate,verbose=True, tensorboard_log=f"{log_dir}/tensorboard/")
