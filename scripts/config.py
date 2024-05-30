1c1d5e89b94de33f24f54f11f40fbf43143157d4
FabriceTUe <f.e.kusters@student.tue.nl>
2024-05-30 18:31:06 +0200

    temp remove pest_aside

--- scripts/config.py
+++ scripts/config.py
@@ -31,14 +31,6 @@ CONFIG = {
         'resize': (160, 144),
         'timesteps': 1000000
     },
-    'pest_aside': {
-        'actions': [['UP'], ['DOWN'], ['LEFT'], ['RIGHT'], ['A'], ['B']],
-        'game_env': 'pest_aside-MP',
-        'state': 'Level1',
-        'clip_reward': False,
-        'resize': (80, 72),
-        'timesteps': 10000000
-    },
     'match-em': {
         'actions': [['UP'], ['DOWN'], ['A']],
         'game_env': 'match-em-MP',
