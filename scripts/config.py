43765a39842852c5c93d6e27e4ce4839b7386052
FabriceTUe <f.e.kusters@student.tue.nl>
2024-08-04 16:18:07 +0200

    added iterations to bunny_belt

--- scripts/config.py
+++ scripts/config.py
@@ -53,7 +53,7 @@ CONFIG = {
         'state': 'Level1',
         'clip_reward': False,
         'resize': (40, 36),
-        'timesteps': 1000000
+        'timesteps': 1000000000000
     },
     'pest_aside': {
         'actions': [['UP'], ['DOWN'], ['LEFT'], ['RIGHT'], ['A'], ['B']],
