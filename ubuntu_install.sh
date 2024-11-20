449190f700a04ea0ba22e0e9f93e2332be903dbf
FabriceTUe <f.e.kusters@student.tue.nl>
2024-11-20 20:46:39 +0100

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -7,5 +7,6 @@ echo "Installing required packages..."
 sudo apt-get update
 sudo apt-get install build-essential
 sudo apt-get install zlib1g-dev
+sudo apt-get install -y python3-opengl
 conda install conda-forge::libcxx
 conda install -c conda-forge libstdcxx-ng
