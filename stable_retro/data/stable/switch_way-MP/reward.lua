4ca83e15ad60b6256225537a1f43fa426c032361
Vincent Dekkers <v.d.n.dekkers@gmail.com>
2024-12-17 11:28:00 +0100

    Updated switch_way

--- stable_retro/data/stable/switch_way-MP/reward.lua
+++ stable_retro/data/stable/switch_way-MP/reward.lua
@@ -3,15 +3,16 @@ y_target = 224
 start_x = 56
 start_y = 1936
 start_distance = math.sqrt((x_target-start_x)^2+(y_target-start_y)^2) --also the best distance
+last_distance = start_distance
 
 function reward()
     local distance = math.sqrt((x_target-data.x)^2+(y_target-data.y)^2)
-    local reward
+    local reward = 0
     if distance < start_distance then
-        reward = 1/distance
+        reward = 0.004*(start_distance - distance) 
         start_distance = distance
-    else
-        reward = -0.00001
     end
+    reward = reward + 0.001*(last_distance - distance)
+    last_distance = distance
     return reward
 end
