8fd5a87e36fbc8480db2623f8a24f917ab8dbbad
Vincent Dekkers <v.d.n.dekkers@gmail.com>
2024-12-19 19:55:31 +0100

    Removed print statement

--- stable_retro/data/stable/broom_zoom-MP/broom_zoom_reward.lua
+++ stable_retro/data/stable/broom_zoom-MP/broom_zoom_reward.lua
@@ -8,9 +8,9 @@ function reward()
         score_rew = 1
         total = total + score_rew
     end
-    if data.game_over == 1 then
-        print(string.format("episode reward: %d", total))
-    end
+    -- if data.game_over == 1 then
+    --     print(string.format("episode reward: %d", total))
+    -- end
     previous_score = data.broom_zoom_score
 
     return score_rew;
