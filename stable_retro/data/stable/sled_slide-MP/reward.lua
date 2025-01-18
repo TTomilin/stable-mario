4ab8c418b5f3645c3bd8e8dd0c662981a9c2c435
Vincent Dekkers <v.d.n.dekkers@gmail.com>
2025-01-18 14:26:50 +0100

    updated sled_slide and cloud_climb

--- stable_retro/data/stable/sled_slide-MP/reward.lua
+++ stable_retro/data/stable/sled_slide-MP/reward.lua
@@ -24,7 +24,7 @@ function reward()
     elseif progress < highestsegment then
         reward = -0.0001
     else
-        reward = 1*(progress - lastsegment) + 0.0000001*(data.speed) + 0.005*(data.speed - previousspeed)
+        reward = 1*(progress - lastsegment) + 0.000001*(data.speed) + 0.005*(data.speed - previousspeed)
         previousspeed = data.speed
         if progress > lastsegment then 
             highestsegment = progress
@@ -33,5 +33,9 @@ function reward()
     end 
     reward = reward / 17
     return reward 
+end
+
+function endcondition()
+    return data.ingame == 0 or math.abs(data.progress - highestsegment) > 1
 end
 
