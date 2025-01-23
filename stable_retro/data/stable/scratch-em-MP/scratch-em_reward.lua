152ce58f5d200043d2faab639afdf6f74aa0aa5a
JuniorJansen <t.j.v.jansen@student.tue.nl>
2025-01-23 10:27:47 +0100

    Update scratch-em variables and reward

--- stable_retro/data/stable/scratch-em-MP/scratch-em_reward.lua
+++ stable_retro/data/stable/scratch-em-MP/scratch-em_reward.lua
@@ -2,9 +2,8 @@ current_coins = 30
 
 function reward()
     local reward = 0
-
     if data.coins > current_coins then
-        reward = data.coins - current_coins
+        reward = (data.coins - current_coins) / 100
         current_coins = data.coins
     end
     return reward
