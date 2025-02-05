249e6ee4fd0e0e5ac4904e25aef6790da30e0119
FabriceTUe <f.e.kusters@student.tue.nl>
2025-02-05 11:00:15 +0100

    Update update.sh

--- src/update.sh
+++ src/update.sh
@@ -3,5 +3,5 @@
 echo "Updating library file..."
 cd ..
 cmake .
-cp build/lib.linux-x86_64-cpython-39/_retro.cpython-39-x86_64-linux-gnu.so scripts/retro/_retro.cpython-39-x86_64-linux-gnu.so
+cp build/lib.linux-x86_64-cpython-39/retro/_retro.cpython-39-x86_64-linux-gnu.so scripts/retro/_retro.cpython-39-x86_64-linux-gnu.so
 cd src
