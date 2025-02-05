ff6764b49d336f8a7d42dea993262e7b631a2faa
FabriceTUe <f.e.kusters@student.tue.nl>
2025-02-05 10:57:33 +0100

    corrected updater

--- src/update.sh
+++ src/update.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
 echo "Updating library file..."
-cmake ..
-cp ../build/lib.linux-x86_64-cpython-39/_retro.cpython-39-x86_64-linux-gnu.so ../scripts/retro/_retro.cpython-39-x86_64-linux-gnu.so
+cd ..
+cmake .
+cp build/lib.linux-x86_64-cpython-39/_retro.cpython-39-x86_64-linux-gnu.so scripts/retro/_retro.cpython-39-x86_64-linux-gnu.so
+cd src
