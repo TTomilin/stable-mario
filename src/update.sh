53d1ac6e0d84d0cc00ac481f9dc0ef914065c348
FabriceTUe <f.e.kusters@student.tue.nl>
2025-02-05 10:53:07 +0100

    Added c++ updater

---
+++ src/update.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+echo "Updating library file..."
+cmake ..
+cp ../build/lib.linux-x86_64-cpython-39/_retro.cpython-39-x86_64-linux-gnu.so ../scripts/retro/_retro.cpython-39-x86_64-linux-gnu.so
