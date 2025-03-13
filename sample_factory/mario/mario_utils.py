0a743794edbed901305dc905d20887006bfd0180
TTomilin <tristan.tomilin@hotmail.com>
2025-03-13 09:47:47 +0100

    Default video recording at 60 fps

--- sample_factory/mario/mario_utils.py
+++ sample_factory/mario/mario_utils.py
@@ -86,6 +86,7 @@ def make_mario_env(env_name, cfg, env_config, render_mode: Optional[str] = None)
     game = CONFIG[cfg.game]
     state = cfg.load_state if cfg.load_state is not None else game["state"]
     env = stable_retro.make(game=game['game_env'], state=state, render_mode=render_mode)
+    env.metadata["render_fps"] = cfg.render_fps
 
     if cfg.discretize:
         env = Discretizer(env, game["actions"])
@@ -111,4 +112,5 @@ def make_mario_env(env_name, cfg, env_config, render_mode: Optional[str] = None)
     env = ResizeObservation(env, game["resize"])
     env = PixelFormatChwWrapper(env)
     # env = FrameStack(env, cfg.env_framestack)  # TODO out of order
+
     return env
